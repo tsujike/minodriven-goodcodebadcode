7c2ec42d018a78b716c492a314eaaaad1e28f7f4
KENZO TSUJI <58035269+tsujike@users.noreply.github.com>
2022-06-17 21:35:27 +0900

    Create Sample6_18
    Javaです

--- Section6/Sample6_18.java
+++ Section6/Sample6_18.java
@@ -1,57 +1,56 @@
 /** enum */
 enum MagicType {
-	fire, // 炎の魔法
-	hellFire; // 地獄の業火
+  fire, // 炎の魔法
+  hellFire; // 地獄の業火
 }
 
 /** Magicクラス */
 class Magic {
-	final String name;
-	final int costMagicPoint;
-	final int attackPower;
+  final String name;
+  final int costMagicPoint;
+  final int attackPower;
 
-	Magic(final MagicType magicType, final Member member) {
-		switch (magicType) {
-		case fire:
-			name = "ファイヤ";
-			costMagicPoint = 2;
-			attackPower = 20 + (int) (member.level * 0.5);
-			break;
-		case hellFire:
-			name = "地獄の業火";
-			costMagicPoint = 16;
-			attackPower = 200 + (int) (member.magicAttack * 0.5 + member.vitality * 2);
-			break;
-		default:
-			throw new IllegalArgumentException();
-		}
-	}
+  Magic(final MagicType magicType, final Member member) {
+    switch (magicType) {
+      case fire:
+        name = "ファイヤ";
+        costMagicPoint = 2;
+        attackPower = 20 + (int) (member.level * 0.5);
+        break;
+      case hellFire:
+        name = "地獄の業火";
+        costMagicPoint = 16;
+        attackPower = 200 + (int) (member.magicAttack * 0.5 + member.vitality * 2);
+	break;
+      default:
+      throw new IllegalArgumentException();
+      }
+    }
 }
 
 /** Memberクラス */
 class Member {
-	String name;
-	int level;
-	int magicAttack;
-	int vitality;
+  String name;
+  int level;
+  int magicAttack;
+  int vitality;
 
-	Member(String name, int level, int magicAttack, int vitality) {
-		this.name = name;
-		this.level = level;
-		this.magicAttack = magicAttack;
-		this.vitality = vitality;
-	}
+  Member(String name, int level, int magicAttack, int vitality) {
+    this.name = name;
+    this.level = level;
+    this.magicAttack = magicAttack;
+    this.vitality = vitality;
+    }
 }
 
 /** 実行用 エントリポイント */
 class Sample6_18 {
-	public static void main(String[] args) {
-		Member member = new Member("つじけ", 10, 4, 20);
-		Magic magic = new Magic(MagicType.hellFire, member);
-
-		System.out.println(member.name + "は" + magic.name + "をとなえた");
-		System.out.println("モンスターに" + magic.attackPower + "のダメージをあたえた");
-		System.out.println("MPを" + magic.costMagicPoint + "消費した");
+  public static void main(String[] args) {
+    Member member = new Member("つじけ", 10, 4, 20);
+    Magic magic = new Magic(MagicType.hellFire, member);
+    System.out.println(member.name + "は" + magic.name + "をとなえた");
+    System.out.println("モンスターに" + magic.attackPower + "のダメージをあたえた");
+    System.out.println("MPを" + magic.costMagicPoint + "消費した");
     //つじけは地獄の業火をとなえた, モンスターに242のダメージをあたえた, MPを16消費した
-	}
+    }
 }
