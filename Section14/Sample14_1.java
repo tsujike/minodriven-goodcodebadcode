3fbc024421a981e81035a8fae43f8a81a8d2fe70
KENZO TSUJI <58035269+tsujike@users.noreply.github.com>
2022-07-06 00:52:07 +0900

    14_2

--- Section14/Sample14_1.java
+++ Section14/Sample14_1.java
@@ -9,22 +9,19 @@ class PurchasePointPayment {
 	final LocalDateTime paymentDateTime; // 購入日時
 
 	PurchasePointPayment(final Customer customer, final Comic comic) {
-		if (customer.isEnabled()) {
-			customerId = customer.id;
-			if (comic.isEnabled()) {
-				comicId = comic.id;
-				if (comic.currentPurchasePoint.amount <= customer.possessionPoint.amount) {
-					consumptionPoint = comic.currentPurchasePoint;
-					paymentDateTime = LocalDateTime.now();
-				} else {
-					throw new RuntimeException("所持ポイントが不足しています。");
-				}
-			} else {
-				throw new IllegalArgumentException("現在取り扱いのできないコミックです。");
-			}
-		} else {
+		if (!customer.isEnabled()) {
 			throw new IllegalArgumentException("有効な購入者ではありません。");
 		}
+		customerId = customer.id;
+		if (!comic.isEnabled()) {
+			throw new IllegalArgumentException("現在取り扱いのできないコミックです。");
+		}
+		comicId = comic.id;
+		if (comic.currentPurchasePoint.amount > customer.possessionPoint.amount) {
+			throw new RuntimeException("所持ポイントが不足しています。");
+		}
+		consumptionPoint = comic.currentPurchasePoint;
+		paymentDateTime = LocalDateTime.now();
 	}
 }
 
